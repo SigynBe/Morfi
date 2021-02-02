3bc4e3a7066fee44746ad1af3037913574847e0c
Rebeca PachecoCorrea <rebecapc@icomp.ufam.edu.br>
2021-02-02 15:09:04 -0400

    Initial Commit

---
+++ Morfi-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Morfi-app
+//
+//  Created by Rebeca Pacheco on 02/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
